cc43aaa0ac302be4a8a4de7286561fd258bdebbb
Gede Dyava S <dyavas21@gmail.com>
2021-11-02 03:22:42 +0700

    home_page

--- uicoffe/lib/pages/home_page.dart
+++ uicoffe/lib/pages/home_page.dart
@@ -5,7 +5,73 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      body: Stack(
+        children: [
+          Image.asset(
+            'assets/BG.png',
+            fit: BoxFit.cover,
+            width: double.infinity,
+          ),
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.only(
+                top: 300,
+              ),
+              child: Text(
+                'Sweet &\nNaise Coffee',
+                style: GoogleFonts.poppins(
+                  fontSize: 24,
+                  fontWeight: FontWeight.w700,
+                  color: Color(0xff023E4A),
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ),
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.only(top: 420),
+              child: Text(
+                'Naise Coffee can change The\natmosphere in the morning',
+                style: GoogleFonts.poppins(
+                  fontSize: 12,
+                  fontWeight: FontWeight.w300,
+                  color: Color(0xff687A79),
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(
+              top: 650,
+            ),
+            child: Center(
+              child: GestureDetector(
+                onTap: () {},
+                child: Container(
+                  width: 260,
+                  height: 55,
+                  decoration: BoxDecoration(
+                    color: Color(0xff023E4A),
+                    borderRadius: BorderRadius.circular(100),
+                  ),
+                  child: Center(
+                    child: Text(
+                      'ORDER NOW',
+                      style: GoogleFonts.poppins(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w600,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
