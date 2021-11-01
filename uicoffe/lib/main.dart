06392aac985da031aae00c86c5f43a2dc33cf94d
Gede Dyava S <dyavas21@gmail.com>
2021-11-02 02:55:10 +0700

    main and home

--- uicoffe/lib/main.dart
+++ uicoffe/lib/main.dart
@@ -1,5 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:uicoffe/pages/home_page.dart';
 
+void main() => runApp(CoffeOrder());
 
-void main => 
+class CoffeOrder extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: {
+        '/': (context) => HomePage(),
+      },
+    );
+  }
+}
--- uicoffe/lib/pages/home_page.dart
+++ uicoffe/lib/pages/home_page.dart
@@ -1,2 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
